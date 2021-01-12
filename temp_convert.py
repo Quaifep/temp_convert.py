9c16a5c6c45981eb82b6e62fd724dc97e57fc214
Paul <quaifep@yahoo.com>
2021-01-12 13:10:33 -0800

    Final (corrected spaces)

--- temp_convert.py
+++ temp_convert.py
@@ -4,5 +4,5 @@
 
 print("Please enter a Celsius temperature.")
 C_temp = float(input())
-F = ( (9 / 5) * C_temp + 32)
-print("The equivalent Fahrenheit temperature is:" , F)
+F = ((9 / 5) * C_temp + 32)
+print("The equivalent Fahrenheit temperature is:", F)

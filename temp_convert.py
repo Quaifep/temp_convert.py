b2d5c18fc779f9f9db771fbb472c3f1825dbac35
Paul <quaifep@yahoo.com>
2021-01-13 17:31:07 -0800

    Changed formatting to be exact.

--- temp_convert.py
+++ temp_convert.py
@@ -3,6 +3,7 @@
 # Description: Converts celsius to Fahrenheit.
 
 print("Please enter a Celsius temperature.")
-C_temp = int(input())
+C_temp = float(input())
 F_temp = ((9 / 5) * C_temp + 32)
-print("The equivalent Fahrenheit temperature is:", F_temp)
+print("The equivalent Fahrenheit temperature is:")
+print(F_temp)

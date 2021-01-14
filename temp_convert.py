744be50c0076753451e15eba4921e36d2fdcde42
Paul <quaifep@yahoo.com>
2021-01-13 16:19:18 -0800

    Changed float(input()) to int(input())

--- temp_convert.py
+++ temp_convert.py
@@ -3,6 +3,6 @@
 # Description: Converts celsius to Fahrenheit.
 
 print("Please enter a Celsius temperature.")
-C_temp = float(input())
-F = ((9 / 5) * C_temp + 32)
-print("The equivalent Fahrenheit temperature is:", F)
+C_temp = int(input())
+F_temp = ((9 / 5) * C_temp + 32)
+print("The equivalent Fahrenheit temperature is:", F_temp)
